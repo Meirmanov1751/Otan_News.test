c9e85bed05a77a9dd0329be93cb84aa8463b3d44
Meirmanov <dau.kassymov@aues.kz>
2024-07-11 14:23:05 +0500

    first

--- Otan_news/settings.py
+++ Otan_news/settings.py
@@ -202,7 +202,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/app/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = '/app/static/'
 
 # Default primary key field type
