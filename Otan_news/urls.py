0401172233d52e7ab3d96da6571c46157fa7bb6a
Meirmanov <dau.kassymov@aues.kz>
2024-07-11 13:27:52 +0500

    first

--- Otan_news/urls.py
+++ Otan_news/urls.py
@@ -3,13 +3,16 @@ from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from django.conf import settings
-from .yasg import urlpatterns as doc_urls
-
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
-    TokenRefreshView, TokenVerifyView,
+    TokenRefreshView,
+    TokenVerifyView,
 )
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+from django.conf import settings
 
 from language.urls import router as post_language
 from news.sitemaps import NewsSitemap
@@ -29,8 +32,20 @@ sitemaps = {
     'quotes': QuoteSitemap,
 }
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="API documentation",
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
-    path('', include(doc_urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('auth/', include('djoser.urls.jwt')),
