940549c342c152e2fb1f5a34b0d85afca60a8024
Meirmanov <dau.kassymov@aues.kz>
2024-07-11 15:25:50 +0500

    first

--- Otan_news/urls.py
+++ Otan_news/urls.py
@@ -43,15 +43,17 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
-    path('auth/', include('djoser.urls.jwt')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+                  path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+                  path('swagger.yaml', schema_view.without_ui(cache_timeout=0), name='schema-yaml'),
+                  path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+                  path('auth/', include('djoser.urls')),
+                  path('auth/', include('djoser.urls.authtoken')),
+                  path('auth/', include('djoser.urls.jwt')),
+                  path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+                  path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+                  path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+                  path('admin/', admin.site.urls),
+                  path('api/', include(router.urls)),
+                  path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
