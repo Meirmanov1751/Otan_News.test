5a1f71525c674f7a324735df6abc05df0d911f06
Meirmanov <dau.kassymov@aues.kz>
2024-07-11 15:51:34 +0500

    first

--- news/filters.py
+++ news/filters.py
@@ -4,13 +4,13 @@ from django_filters import rest_framework as filters
 from .models import News, Tag, Comment, Quote, Subscriber
 
 class NewsFilter(filters.FilterSet):
-    title = django_filters.CharFilter(lookup_expr='icontains')
+    category = filters.CharFilter(lookup_expr='icontains')
     tags = django_filters.CharFilter(field_name='tags__name', lookup_expr='icontains')
     comments = django_filters.CharFilter(field_name='comments__content', lookup_expr='icontains')
 
     class Meta:
         model = News
-        fields = '__all__'
+        fields = ['category', 'tags', 'comments']
 
 class CommentFilter(filters.FilterSet):
     content = django_filters.CharFilter(lookup_expr='icontains')
