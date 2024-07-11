0e8bfc7fad934f87d2e75dc5927fbba54f847ec5
Meirmanov <dau.kassymov@aues.kz>
2024-07-11 15:49:21 +0500

    first

--- Otan_news/settings.py
+++ Otan_news/settings.py
@@ -206,9 +206,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = '/app/static/'
 
-STATICFILES_DIRS = (
-    '/app/staticfiles/',
-)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
