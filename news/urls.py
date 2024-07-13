73e18345bd692c01e1457d421a7d935ee8016dc4
Meirmanov <dau.kassymov@aues.kz>
2024-07-13 12:48:01 +0500

    first

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import NewsViewSet, CommentViewSet, NewsShortViewSet
+from .views import NewsViewSet, CommentViewSet, NewsShortViewSet, CommentCreateViewSet
 
 router = DefaultRouter()
 
 router.register('news', NewsViewSet, basename='news')
 router.register('news_short', NewsShortViewSet, basename='news_short')
 router.register(r'comments', CommentViewSet)
+router.register(r'commentsCreate', CommentCreateViewSet)
