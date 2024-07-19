faca9c36b26baaa441a3f721e598baeb59fcf8e4
Meirmanov <dau.kassymov@aues.kz>
2024-07-19 10:24:57 +0500

    first

--- Otan_news/settings.py
+++ Otan_news/settings.py
@@ -231,7 +231,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'user.User'
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     # Use Django's standard django.contrib.auth permissions,
