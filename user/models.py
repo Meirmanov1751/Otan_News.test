40f9bafcfb3fff6010cdcf457080132417daaadb
Meirmanov <dau.kassymov@aues.kz>
2024-08-16 15:17:20 +0500

    16.08.2024

--- user/models.py
+++ user/models.py
@@ -13,11 +13,9 @@ class UserManager(BaseUserManager):
         other_fields.setdefault('is_active', True)
 
         if other_fields.get('is_staff') is not True:
-            raise ValueError(
-                'Superuser must be assigned to is_staff=True.')
+            raise ValueError('Superuser must be assigned to is_staff=True.')
         if other_fields.get('is_superuser') is not True:
-            raise ValueError(
-                'Superuser must be assigned to is_superuser=True.')
+            raise ValueError('Superuser must be assigned to is_superuser=True.')
 
         return self.create_user(email, password, **other_fields)
 
@@ -27,6 +25,11 @@ class UserManager(BaseUserManager):
             raise ValueError(_('You must provide an email address'))
 
         email = self.normalize_email(email)
+
+        # Проверяем роль пользователя и устанавливаем is_active = True, если роль журналист или супер админ
+        if other_fields.get('role') in [User.ROLES.JOURNALIST, User.ROLES.SUPER_ADMIN]:
+            other_fields.setdefault('is_active', True)
+
         user = self.model(email=email, **other_fields)
         user.set_password(password)
         user.save()
