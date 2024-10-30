37821232d94a64b3d9582d924d6d1f950ccc5c73
Meirmanov <dau.kassymov@aues.kz>
2024-10-30 18:58:07 +0500

    30.10.2024

--- news/models.py
+++ news/models.py
@@ -84,7 +84,7 @@ class News(models.Model):
 
 class NewsImage(models.Model):
     news = models.ForeignKey(News, on_delete=models.CASCADE, related_name='images', blank=True, null=True)
-    image = models.ImageField(upload_to='news/images/', blank=True, null=True)
+    image = models.ImageField(upload_to='newsNew/images/', blank=True, null=True)
     order = models.PositiveIntegerField(default=0, blank=True, null=True)
     source_url = models.URLField(max_length=255, blank=True, null=True)
     alt = models.CharField(max_length=255, blank=True, null=True)
