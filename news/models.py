2bab3a852337cb400ad2368967efe69e048a3be9
Meirmanov <dau.kassymov@aues.kz>
2024-11-06 12:19:44 +0500

    12

--- news/models.py
+++ news/models.py
@@ -70,7 +70,7 @@ class News(models.Model):
     is_published = models.BooleanField(default=False)
     published_at = models.DateTimeField(blank=True, null=True)
     views = models.PositiveIntegerField(default=0)
-    link_to_source = models.TextField()
+    link_to_source = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f"Новость {self.id}"
