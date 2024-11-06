50ada0be3c551fce6129f19f25500b776a2af118
Meirmanov <dau.kassymov@aues.kz>
2024-11-06 14:57:01 +0500

    12

--- news/models.py
+++ news/models.py
@@ -88,7 +88,7 @@ class NewsCover(models.Model):
     news = models.ForeignKey(News, on_delete=models.CASCADE, related_name='covers', blank=True, null=True)
     cover = models.ImageField(upload_to='news/covers/', blank=True, null=True)
     order = models.PositiveIntegerField(default=0, blank=True, null=True)
-    source_url = models.TextField()
+    source_url = models.TextField(blank=True, null=True)
     alt = models.CharField(max_length=255, blank=True, null=True)
 
 
