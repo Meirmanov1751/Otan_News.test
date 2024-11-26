e4e38cb6f7c7492bfa81f5aac635246fc1230d7a
Meirmanov <dau.kassymov@aues.kz>
2024-11-26 15:57:56 +0500

    26.11.2024

--- news/filters.py
+++ news/filters.py
@@ -11,10 +11,11 @@ class NewsFilter(filters.FilterSet):
     is_published = filters.BooleanFilter()
     tags = django_filters.CharFilter(field_name='tags__name', lookup_expr='icontains')
     comments = django_filters.CharFilter(field_name='comments__content', lookup_expr='icontains')
+    title = filters.CharFilter(lookup_expr='icontains')  # Фильтр по title
 
     class Meta:
         model = News
-        fields = ['category', 'subcategory', 'exclusive', 'tags', 'comments', 'is_published']
+        fields = ['category', 'subcategory', 'exclusive', 'tags', 'comments', 'is_published', 'title']
 
 
 class CommentFilter(filters.FilterSet):
